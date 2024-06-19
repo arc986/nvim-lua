c1590ae839255433b7ee72296cce773a9e829b1a
arc986 <19562405+arc986@users.noreply.github.com>
2024-06-18 20:46:45 -0500

    neo tree mejoras

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,10 +9,32 @@ return{
     },
     config = function()
         require("neo-tree").setup({
+            popup_border_style = "rounded",
+            enable_git_status = true,
+            enable_diagnostics = true,
             source_selector = {
                 winbar = true,
-                statusline = true
-            }
+                statusline = true,
+                sources = {
+                    { source = "filesystem" },
+                    { source = "buffers" },
+                    { source = "git_status" },
+                  },
+            },
+            indent = {
+                indent_size = 2,
+                padding = 1, -- extra padding on left hand side
+                -- indent guides
+                with_markers = true,
+                indent_marker = "│",
+                last_indent_marker = "└",
+                highlight = "NeoTreeIndentMarker",
+                -- expander config, needed for nesting files
+                with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+                expander_collapsed = "",
+                expander_expanded = "",
+                expander_highlight = "NeoTreeExpander",
+              },
         })
         vim.api.nvim_exec([[autocmd BufEnter * lcd %:p:h]], false)
         local map = vim.api.nvim_set_keymap
